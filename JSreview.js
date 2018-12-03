b4143913de9d23385c6811c66d75913877b1d1a4
Kevin Yu <learn@learn15.local>
2018-12-03 14:21:20 -0800

    adding second problem to review work.

--- JSreview.js
+++ JSreview.js
@@ -26,20 +26,48 @@
 // hint: use the .includes() method
 
 // two arrays
-var arr1 = [1, 2, 3, 4]
-var arr2 = [1, 3, 5, 7]
+// var arr1 = [1, 2, 3, 4]
+// var arr2 = [1, 3, 5, 7]
+//
+// function dupCheck(objOne, objTwo) {
+// var arr3 = []
+// var arr4 = []
+//
+// for (let i = 0; i < objOne.length; i++) {
+//   if (objTwo.includes(objOne[i]) === false) {
+//     arr3.push(objOne[i])
+//   }
+// } arr4 = arr3.concat(objTwo)
+//     return arr4 }
+//
+// console.log(dupCheck(arr1, arr2))
 
-// 1, 2, 3, 4, 5, 7
 
-function dupCheck(objOne, objTwo) {
-var arr3 = []
-var arr4 = []
+//Create a function that takes in a sentence and returns only the consonants (removes the vowels).
 
-for (let i = 0; i < objOne.length; i++) {
-  if (objTwo.includes(objOne[i]) === false) {
-    arr3.push(objOne[i])
+// hint: you will need a loop
+// hint: use the .includes() method
+
+var testSentence1 = "Toma Carajo!"
+//expected output = “Ths wbst s wsm!”
+
+function removeVowels(testSentence1) {
+
+var vowels = ["a", "e", "i", "o", "u"]
+var splitSentence = testSentence1.split("")
+var consonants = []
+
+// for (let i = 0; i < testSentence1.length; i++){
+//   if (testSentence1.includes(vowels[i]) === false) {
+//     consonants.push(testSentence1[i])
+//   }
+
+for (let i = 0; i < splitSentence.length; i++) {
+  //splitsentence and vowels are swapped, change them on next line
+  if (!vowels.includes(splitSentence[i])) {
+    consonants.push(splitSentence[i])
   }
-} arr4 = arr3.concat(objTwo)
-    return arr4 }
+}   return(consonants)
+}
 
-console.log(dupCheck(arr1, arr2))
+console.log(removeVowels(testSentence1))
